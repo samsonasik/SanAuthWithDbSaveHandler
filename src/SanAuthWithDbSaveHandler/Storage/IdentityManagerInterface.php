ad8cf547243472482615173317aee930b3b69b5b
Lucas CORBEAUX <lucas.corbeaux@gmail.com>
2013-07-29 12:06:47 +0200

    Authentication refactoring implementing a simpe Facade pattern for clarifying code intents.

---
+++ src/SanAuthWithDbSaveHandler/Storage/IdentityManagerInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace SanAuthWithDbSaveHandler\Storage;
+
+interface IdentityManagerInterface
+{
+    public function login($identity, $credential);
+    public function logout();
+    public function hasIdentity();
+    public function storeIdentity(array $identity);
+}
