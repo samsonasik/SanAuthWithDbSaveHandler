e2ae4dfcebb7a952d7b1adaadcf6496c994423b9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-10-22 07:53:40 +0700

    add route event to check if not authenticated, then go to auth page

--- Module.php
+++ Module.php
@@ -2,8 +2,38 @@
 //filename : SanAuthWithDbSaveHandler/Module.php
 namespace SanAuthWithDbSaveHandler;
 
+use Zend\Mvc\MvcEvent;
+
 class Module
 {
+    public function onBootstrap(MvcEvent $e)
+    {
+        $em = $e->getApplication()->getEventManager();
+        $em->attach('route', array($this, 'checkAuthenticated'));
+    }
+
+    public function isOpenRequest(MvcEvent $e)
+    {
+        if ($e->getRouteMatch()->getParam('controller') == 'SanAuthWithDbSaveHandler\Controller\AuthController') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkAuthenticated(MvcEvent $e)
+    {
+        if (!$this->isOpenRequest($e)) {
+            $sm = $e->getApplication()->getServiceManager();
+            if (! $sm->get('AuthService')->getStorage()->getSessionManager()
+                        ->getSaveHandler()->read($sm->get('AuthService')->getStorage()->getSessionId())) {
+                $e->getRouteMatch()
+                    ->setParam('controller', 'SanAuthWithDbSaveHandler\Controller\Auth')
+                    ->setParam('action', 'index');
+            }
+        }
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
